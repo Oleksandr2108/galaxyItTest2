fd2e8466684e99fa2d1f3aeac8651c45737c539a
Oleksandr Yepik <sepika534@gmail.com>
2026-01-06 13:04:24 +0200

    feat: add Title components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Header from "@/components/Header/Header";
+import Title from "@/components/Title/Title";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Title
+        title="Authentic recipes"
+        highlight={[1]}
+        italic={[1]}
+        subtitle="Try one of our pearl sugar recipe at the comfort of your home.
+In the kitchen with waffle pantry."
+      />
     </>
   );
 }
---
+++ components/Title/Title.tsx
@@ -0,0 +1,44 @@
+import { spawn } from "child_process";
+import styles from "./Title.module.scss";
+
+interface TitleProps {
+  title: string;
+  highlight?: number[];
+  italic?: number[];
+  subtitle?: string;
+}
+
+const Title = ({
+  title,
+  highlight = [],
+  italic = [],
+  subtitle,
+}: TitleProps) => {
+  const words = title.split(" ");
+  return (
+    <>
+      <h1 className={styles.title}>
+        {words.map((word, index) => {
+          const isHighlight = highlight.includes(index);
+          const isItalic = italic.includes(index);
+
+          return (
+            <span
+              key={index}
+              className={[
+                isHighlight ? styles.highlight : "",
+                isItalic ? styles.italic : "",
+              ].join(" ")}
+            >
+              {word}{" "}
+            </span>
+          );
+        })}
+      </h1>
+
+      <p className={styles.subtitle}>{subtitle}</p>
+    </>
+  );
+};
+
+export default Title;
