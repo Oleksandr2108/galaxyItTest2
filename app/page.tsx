0f6792f014030e978228a545c142c2ebde7959ef
Oleksandr Yepik <sepika534@gmail.com>
2026-01-06 14:37:52 +0200

    feat: add recipes card components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import Header from "@/components/Header/Header";
+import ResipesCard from "@/components/ResipesCard/ResipesCard";
 import Title from "@/components/Title/Title";
+import mainImage from "@/assets/images/Recipe1.jpg";
 
 const Home = () => {
-  return(
+  return (
     <>
-    
+      <Header />
+      <ResipesCard
+        mainImage={mainImage}
+        rating={2.5}
+        title="traditional liege waffle"
+        time="45 min"
+        description="Try one of our pearl sugar recipe at the comfort of your home. In the kitchen with waffle pantry."
+      />
     </>
-  )
-}
+  );
+};
 
 export default Home;
--- components/ResipesCard/ResipesCard.tsx
+++ components/ResipesCard/ResipesCard.tsx
@@ -1,8 +1,10 @@
 import Image, { StaticImageData } from "next/image";
 import StarRating from "../StarRating/StarRating";
 
-import Time from '@/assets/images/Time.svg'
-import Share from "@/assets/images/Vector.svg"
+import Time from "@/assets/images/Time.svg";
+import Share from "@/assets/images/Vector.svg";
+
+import styles from "./ResipesCard.module.scss";
 
 interface RecipesCardProps {
   mainImage: string | StaticImageData;
@@ -20,26 +22,37 @@ const ResipesCard = ({
   description,
 }: RecipesCardProps) => {
   return (
-    <div>
-      <Image src={mainImage}  width={472} height={315} alt="image result"/>
-      <div>
-        {/* Stars */}
+    <div className={styles.wrapper}>
+      <Image
+        src={mainImage}
+        width={472}
+        height={315}
+        alt="image result"
+      />
+      <div className={styles.wrapper__info}>
         <StarRating rating={rating} />
-        {/* Title */}
-        <span>{title}</span>
-        {/* Time */}
-        <div> 
-          <Image src={Time} width={20} height={20}  alt="time icon"/>
+
+        <span className={styles["wrapper__info-title"]}>{title}</span>
+
+        <div className={styles["wrapper__info-time"]}>
+          <Image
+            src={Time}
+            width={20}
+            height={20}
+            alt="time icon"
+          />
           <p>{time}</p>
         </div>
 
-        {/* Description */}
-        <p>{description}</p>
+        <p className={styles["wrapper__info-description"]}>{description}</p>
 
-        {/* LinkIcon */}
-        <div>
-
-        <Image src={Share} width={20} height={23} alt="share icon" />
+        <div className={styles["wrapper__info-share"]}>
+          <Image
+            src={Share}
+            width={20}
+            height={23}
+            alt="share icon"
+          />
         </div>
       </div>
     </div>
